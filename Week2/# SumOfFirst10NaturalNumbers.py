3a2a0a209b20bf315c8962c62a29a17543b84a9d
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-02 12:06:12 +0530

    Create # SumOfFirst10NaturalNumbers.py

---
+++ Week2/# SumOfFirst10NaturalNumbers.py
@@ -0,0 +1,19 @@
+#i) For Loop :
+sum = 0
+for i in range (1,11):
+    sum += i
+print("Sum of First 10 Natural numbers is : ",sum)
+
+# output
+Sum of First 10 Natural numbers is :  55
+
+#ii) While Loop:
+i = 0
+sum = 0
+while i < 11 :
+    sum += i
+    i += 1
+print("Sum of First 10 Natural numbers is : ",sum)
+
+#output
+Sum of First 10 Natural numbers is :  55
