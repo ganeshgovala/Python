efdfd8d307269ad9273288e1a2b385cab6cb4703
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-02 12:09:25 +0530

    Create MultiplicationTable.py

---
+++ Week2/MultiplicationTable.py
@@ -0,0 +1,37 @@
+#i) For Loop :
+n = int(input("Enter the number : "))
+for i in range (1, 11) :
+    print(n,"X",i,"=",n*i)
+
+#output
+Enter the number : 5
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+
+#ii) While Loop :
+n = int(input("Enter the number : "))
+i = 1
+while i < 11 : 
+    print(n,"X",i,"=",n*i)
+    i += 1
+  
+#output
+Enter the number : 5
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
