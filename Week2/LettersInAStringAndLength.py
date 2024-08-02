c3964191502c0c960b824d94b6467b9c6646bdfd
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-02 12:10:56 +0530

    Create LettersInAStringAndLength.py

---
+++ Week2/LettersInAStringAndLength.py
@@ -0,0 +1,16 @@
+ch = input("Enter the String : ")
+length = 0
+for i in ch :
+    print(i)
+    length += 1
+print("Length of the string is : ", length)
+
+#output
+Enter the String : vishnu
+v
+i
+s
+h
+n
+u
+Length of the string is :  6
