7f3729d87245a477fc83d531bc29bfa3849e26b4
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-23 10:31:40 +0530

    Create ConcatinationOfTwoLists.py

---
+++ Week5/ConcatinationOfTwoLists.py
@@ -0,0 +1,9 @@
+li1 = ["M", "na", "i", "Abhi"]
+li2 = ["y", "me", "s", "Ram"]
+res = []
+for i in range(len(li1)) :
+    res.append(li1[i] + li2[i])
+print(res)
+
+#output
+['My', 'name', 'is', 'AbhiRam']
