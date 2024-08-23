42ccff311e8802906631fc5c9237953650d642ef
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-23 10:17:56 +0530

    Update ReverseAList.py

--- Week5/ReverseAList.py
+++ Week5/ReverseAList.py
@@ -5,3 +5,13 @@ for i in range(n) :
     li.append(int(input("Enter element : ")))
 print("Before Reversing :", li)
 print("After Reversing :",li[::-1])
+
+#output
+Enter no.of elements : 5
+Enter element : 1
+Enter element : 2
+Enter element : 3
+Enter element : 4
+Enter element : 5
+Before Reversing : [1, 2, 3, 4, 5]
+After Reversing : [5, 4, 3, 2, 1]
