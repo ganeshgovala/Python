eb6c51aa883f15cc4e7f9f2a558520764d750fa6
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-09-20 11:18:37 +0530

    Update reverseList.py

--- Week7/reverseList.py
+++ Week7/reverseList.py
@@ -1,11 +1,6 @@
 def reverseList(li) :
-    start = 0
-    end = len(li) - 1
-    while(start < end) :
-        temp = li[start]
-        li[start] = li[end]
-        li[end] = temp
-        start += 1
-        end -= 1
-    return li
+    res = []
+    for i in li :
+        res.insert(0, i)
+    return res
 print(reverseList([1,2,3,4,5,6,7,8]))
