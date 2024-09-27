518a41c45ca6b1e19f879708382c4a493326d25e
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-09-27 11:24:05 +0530

    Create add_keyVaulePair.py

---
+++ Week8/add_keyVaulePair.py
@@ -0,0 +1,14 @@
+thisDict = {
+    "name" : "Ganesh",
+    "age" : 18
+}
+
+key = input("Enter key name : ")
+value = input("Enter value : ")
+thisDict[key] = value
+print(thisDict)
+
+output : 
+Enter key name : marks
+Enter value : 85
+{'name': 'Ganesh', 'age': 18, 'marks': '85'}
